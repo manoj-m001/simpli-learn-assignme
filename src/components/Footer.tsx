2db8f53c7bbbfe87a12d8e3cc59a7846701622fb
manoj-netizenn <mallappanavarmanoj@gmail.com>
2026-01-19 23:47:38 +0530

    left padding in deployment issue footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,19 +10,14 @@ export default function Footer() {
         backgroundPosition: "center",
       }}
     >
-      {/* Dark overlay */}
       <div className="absolute inset-0 bg-black/50" />
 
       <div className="relative max-w-7xl mx-auto px-6 py-20">
-        {/* Top content */}
           <p className="text-xl font-semibold mb-4" style={{paddingLeft: "18px"}} >Space is limited.</p>
-
-        {/* RSVP Form */}
 <RsvpForm
   className="flex flex-col sm:flex-row gap-4 max-w-xl mb-16 bg-grey-900/50 p-6 rounded-md"
 />
 
-        {/* Bottom row */}
         <div className="flex flex-col sm:flex-row justify-between items-center gap-6 ">
           {/* Logo */}
           <Image
